13191f48889a5ed51da0c87db7be80ea7ffc66f2
Ilya Maslyanyi <maslanyjila@gmail.com>
2025-05-25 00:24:22 +0300

    update => hibernate core connect to DB // hid confiditional information

--- src/main/java/com/example/MaslyakBank_Token/MaslyakBankTokenApplication.java
+++ src/main/java/com/example/MaslyakBank_Token/MaslyakBankTokenApplication.java
@@ -1,5 +1,6 @@
 package com.example.MaslyakBank_Token;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
