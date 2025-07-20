200c5168774b27ed3e067df4e9725cfa470c9eae
Maslyak <129599445+MrMaslyak@users.noreply.github.com>
2025-07-21 01:15:20 +0300

    logic check exist account with currency

--- MaslyakBank_Account/src/main/java/MaslyakBank_Account/dao/AccountDAO.java
+++ MaslyakBank_Account/src/main/java/MaslyakBank_Account/dao/AccountDAO.java
@@ -2,11 +2,14 @@ package MaslyakBank_Account.dao;
 
 
 import MaslyakBank_Account.entity.AccountTable;
+import enums.Currency;
 import lombok.AllArgsConstructor;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
 import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
 
 @Repository
 @AllArgsConstructor
@@ -34,4 +37,28 @@ public class AccountDAO {
             }
         }
     }
+
+    public AccountTable findByCurrency(UUID userId, Currency currency) {
+        Session session = null;
+        Transaction transaction = null;
+        try {
+            session = sessionFactory.openSession();
+            transaction = session.beginTransaction();
+
+            AccountTable account = session.createQuery(
+                            "FROM AccountTable WHERE currency = :currency AND user.id = :userId", AccountTable.class)
+                    .setParameter("currency", currency)
+                    .setParameter("userId", userId)
+                    .uniqueResult();
+
+            transaction.commit();
+            return account;
+        } catch (Exception e) {
+            if (transaction != null) transaction.rollback();
+            throw e;
+        } finally {
+            if (session != null) session.close();
+        }
+    }
+
 }
--- MaslyakBank_Account/src/main/java/MaslyakBank_Account/system/account/AccountSystem.java
+++ MaslyakBank_Account/src/main/java/MaslyakBank_Account/system/account/AccountSystem.java
@@ -1,8 +1,24 @@
 package MaslyakBank_Account.system.account;
 
+import MaslyakBank_Account.dao.AccountDAO;
+import MaslyakBank_Account.dto.AccountRequestDTO;
+import MaslyakBank_Account.entity.AccountTable;
+import entity.UsersTable;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
 @Component
+@RequiredArgsConstructor
 public class AccountSystem {
 
+    private final AccountDAO accountDAO;
+
+    public  boolean isExistAccount(UsersTable user, AccountRequestDTO dto){
+        AccountTable existingAccount = accountDAO.findByCurrency(user.getId(), dto.getCurrency());
+        if (existingAccount != null) {
+            return true;
+        }
+        // Если нет — создаём новый счёт //todo
+        return false;
+    }
 }
