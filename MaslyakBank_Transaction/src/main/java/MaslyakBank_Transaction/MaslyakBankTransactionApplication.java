f04194b090ad0d5ca803d05e2a5e61e025c8ad97
Maslyak <129599445+MrMaslyak@users.noreply.github.com>
2025-08-27 22:12:21 +0300

    try to deploy transaction module

--- MaslyakBank_Transaction/src/main/java/MaslyakBank_Transaction/MaslyakBankTransactionApplication.java
+++ MaslyakBank_Transaction/src/main/java/MaslyakBank_Transaction/MaslyakBankTransactionApplication.java
@@ -2,8 +2,32 @@ package MaslyakBank_Transaction;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@ComponentScan(basePackages = {
+		"MaslyakBank_Transaction",
+		"MaslyakBank_Account",
+		"MaslyakBank_Model",
+		"MaslyakBank_SecurityKernel",
+		"details",
+		"util",
+		"service",
+		"system",
+		"dto",
+		"filter",
+		"dao",
+		"controller",
+		"repository",
+		"entity",
+		"enums"
+})
+@PropertySource("classpath:/application_transaction.properties")
+@EntityScan({"MaslyakBank_Transaction.entity", "entity", "MaslyakBank_Account.entity"})
+@EnableScheduling
 public class MaslyakBankTransactionApplication {
 
 	public static void main(String[] args) {
