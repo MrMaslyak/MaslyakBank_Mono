44b0be85f4cbcc0cca0266ed9c684cc0bee3d2cd
Maslyak <129599445+MrMaslyak@users.noreply.github.com>
2025-08-29 23:33:03 +0300

    safe

---
+++ MaslyakBank_Transaction/src/main/java/MaslyakBank_Transaction/service/TransactionService.java
@@ -0,0 +1,19 @@
+package MaslyakBank_Transaction.service;
+
+
+import MaslyakBank_Transaction.dto.TransferCardToCardDTO;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class TransactionService {
+
+
+
+
+
+    public void transferCardToCard(TransferCardToCardDTO dto) {
+
+    }
+}
